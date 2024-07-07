c5aaac5d15e93c39e5084edae81f176200336761
Takahashi-rei08 <lingg7074@gmail.com>
2024-07-07 16:08:50 +0900

    test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,8 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        $test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql();
-        dd($test);
+        //$test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql();
+        //dd($test); //DB確認用
         
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
