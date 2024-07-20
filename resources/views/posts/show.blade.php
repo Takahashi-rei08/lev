9121c2cee94898dce31a97999ca1c2f49a7fbd24
Takahashi-rei08 <lingg7074@gmail.com>
2024-07-20 16:17:10 +0900

    add login

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,21 +9,27 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     </head>
-    <body class="antialiased">
-        <h1 class='title'>
-            {{ $post->title }}
-        </h1>
-        <div class='edit'>
-            <a href='/posts/{{ $post->id }}/edit'>edit</a>
-        </div>
-        <div class='content'>
-            <div class='content_post'>
-                <h3>本文</h2>
-                <p class='body'>{{ $post->body }}</p>
+    <x-app-layout>
+    <x-slot name="header">
+        show
+    </x-slot>
+        <body class="antialiased">
+            <h1 class='title'>
+                {{ $post->title }}
+            </h1>
+            <div class='edit'>
+                <a href='/posts/{{ $post->id }}/edit'>edit</a>
             </div>
-        </div>
-        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-        <div class='footer'>
-            <a href='/'>戻る</a>
-        </div>
-    </body>
+            <div class='content'>
+                <div class='content_post'>
+                    <h3>本文</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            </div>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+            <div class='footer'>
+                <a href='/'>戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
+</html>
